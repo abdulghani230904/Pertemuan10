46074266eefcb36eba8629f47f00de9f109900e5
abdulghani230904 <165791823+abdulghani230904@users.noreply.github.com>
2024-11-09 02:32:19 +0700

    Add files via upload

---
+++ InputDataMhs.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan.pkg9;
+
+import java.util.ArrayList;
+import pertemuan.pkg9.Mahasiswa;
+
+/**
+ *
+ * @author Irzi Rhmtllh
+ */
+public class InputDataMhs {
+    ArrayList<Mahasiswa> listMahasiswa;
+
+    public InputDataMhs() {
+        listMahasiswa = new ArrayList();
+    };
+    
+    public void insertData(String nim, String name, String address, String mataKuliah, double[] arrNilai) {
+        Mahasiswa newMhs = new Mahasiswa(nim, name, name, mataKuliah, arrNilai);
+        listMahasiswa.add(newMhs);
+    };
+    
+    public void deleteData(int idx) {  
+      listMahasiswa.remove(idx);
+    };
+    
+    public ArrayList<Mahasiswa> getAllData() {
+        return listMahasiswa;
+    };
+}
